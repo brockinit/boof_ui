a8cd2a9c689e7c63b92c8e1d99ba8307a4c8d6a4
Andrea <takamiya.andrea@gmail.com>
2017-05-16 20:41:38 -1000

    Saving before trying to yarn eject

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,14 +1,6 @@
 module.exports = {
-    "extends": "airbnb",
-    "installedESLint": true,
+    "extends": "airbnb-base",
     "plugins": [
-        "react",
-        "jsx-a11y",
         "import"
-    ],
-    "rules": {
-      "arrow-body-style": "off",
-      "react/jsx-filename-extension": "off",
-      "jsx-a11y/img-has-alt": "off"
-    }
+    ]
 };
