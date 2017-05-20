34d2d0248bdfd9618e466323535557f67e0fe9cd
Andrea <takamiya.andrea@gmail.com>
2017-05-19 22:10:51 -1000

    Added Tabs to the header, needs to be hooked up to routes and styled more

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import { Tabs, Tab } from 'material-ui/Tabs';
 
 const NavBar = () => {
   return (
+    <div>
     <AppBar
       title="Better Odds of Football"
       className="navigation-bar"
+      showMenuIconButton={false}
       style={{backgroundColor: '#fffff'}}
       titleStyle={{color: '#000000'}}
-
+      zDepth={0}
     />
+    <Tabs
+      className="menu-items"
+      contentContainerClassName="menu-items-container"
+      >
+      <Tab label="Articles" />
+      <Tab label="Chart Porn" />
+      <Tab label="About Us" />
+    </Tabs>
+    </div>
   );
 };
 
