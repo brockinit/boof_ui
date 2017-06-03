308ccb7152e31bae4ddeedfeab4157c83805f671
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-06-03 09:21:32 -1000

    Updated format of index.js to use the updated react-router version

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import BoofRouter from './routes';
+import App from './App';
 import './index.css';
 
 injectTapEventPlugin();
 
 ReactDOM.render(
-  <MuiThemeProvider>
-    <BoofRouter />
-  </MuiThemeProvider>,
+  <App />
   document.getElementById('root'),
 );
