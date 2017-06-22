8a45b2448c0d45d53f7ee2d67b6e6cb819145039
brockinit <brocklanoza@gmail.com>
2017-06-21 21:35:08 -1000

    Add marked and set grid using articles

--- client/src/containers/HomepageContainer/index.js
+++ client/src/containers/HomepageContainer/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {GridList, GridTile} from 'material-ui/GridList';
+import { GridList, GridTile } from 'material-ui/GridList';
 import NavBar from '../../components/NavBar';
+import marked from 'marked';
 import { createClient } from 'contentful';
 
 const styles = {
@@ -20,41 +21,6 @@ const styles = {
   },
 };
 
-const tilesData = [
-  {
-    img: 'https://s3.amazonaws.com/web-ready-andrea/nepal-bells.png',
-    title: 'Why are the Browns Sooooo Bad',
-    author: 'jill111',
-    featured: true,
-  },
-  {
-    img: 'https://s3.amazonaws.com/web-ready-andrea/nepal-mt-view.png',
-    title: 'Are you Ready',
-    author: 'Danson67',
-  },
-  {
-    img: 'https://s3.amazonaws.com/web-ready-andrea/japan-temple.png',
-    title: 'To Receive the Knowing',
-    author: 'fancycrave1',
-  },
-  {
-    img: 'https://s3.amazonaws.com/web-ready-andrea/portugal-blue-house.png',
-    title: 'Top Quarterback Picks for your 2017 Draft',
-    author: 'Hans',
-    featured: true,
-  },
-  {
-    img: 'https://s3.amazonaws.com/web-ready-andrea/thailand-boats.png',
-    title: 'Vegetables',
-    author: 'jill111',
-  },
-  {
-    img: 'https://s3.amazonaws.com/web-ready-andrea/japan-many-boards.png',
-    title: 'Water plant',
-    author: 'BkrmadtyaKarki',
-  },
-];
-
 const SPACE_ID = 'wb0iqsd023ks'
 const ACCESS_TOKEN = '943872b949f9300a341513cc498473efe36b1c8fdffe9f1886b18606bd1363cc'
 
@@ -73,6 +39,7 @@ class GridListHome extends React.Component {
       accessToken: ACCESS_TOKEN
     });
     this.getArticles = this.getArticles.bind(this);
+    this.parseMarkdown = this.parseMarkdown.bind(this);
     this.state = {
       articles: []
     };
@@ -100,6 +67,12 @@ class GridListHome extends React.Component {
     });
   }
 
+  parseMarkdown(markdown) {
+    return {
+      __html: marked(markdown, { sanitize: true })
+    };
+  }
+
   render() {
     console.log('this.state.articles', this.state.articles);
     return (
@@ -117,19 +90,21 @@ class GridListHome extends React.Component {
             padding={15}
             style={styles.gridList}
           >
-            {tilesData.map((tile) => (
-              <GridTile
-                key={tile.img}
-                title={tile.title}
-                actionPosition="left"
-                titlePosition="bottom"
-                titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
-                cols={tile.featured ? 2 : 1}
-                rows={tile.featured ? 2 : 1}
-              >
-                <img src={tile.img} />
-              </GridTile>
-            ))}
+            {this.state.articles.map(({ fields, sys }, index) => {
+              return (
+                <GridTile
+                  key={sys.id}
+                  title={fields.articleTitle}
+                  actionPosition="left"
+                  titlePosition="bottom"
+                  titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+                  cols={index % 2 === 0 ? 2 : 1}
+                  rows={index % 2 === 0 ? 2 : 1}
+                >
+                  {/*<img src={tile.img} />*/}
+                </GridTile>
+              );
+            })}
           </GridList>
         </div>
       </div>
