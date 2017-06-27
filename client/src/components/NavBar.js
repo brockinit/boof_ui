567cf2f6407d55f754e6f53f6b855eb2f6d35f48
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-06-26 21:18:22 -1000

    Added the right side Nav Bar items

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import RightSideMenu from './RightSideMenu'
 
 const NavBar = () => {
   return (
     <AppBar
-      title="Title"
+      title="Better Odds of Football"
+      showMenuIconButton={false}
       iconClassNameRight="muidocs-icon-navigation-expand-more"
+      iconElementRight={<RightSideMenu />}
     />
 )};
 
