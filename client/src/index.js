45d9b82f09412bcd150f58f473ef0b3a10176b3b
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-06-26 21:43:53 -1000

    Added reset-css to get rid of the tiny space above the Nav Bar

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
-import './index.css';
+import 'reset-css/reset.css';
 
 injectTapEventPlugin();
 
