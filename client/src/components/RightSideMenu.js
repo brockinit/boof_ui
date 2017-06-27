7166a284fc3fc9e707090ae6d7ac6efd9861b900
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-06-27 07:30:21 -1000

    Font change for the right side menu items

--- client/src/components/RightSideMenu.js
+++ client/src/components/RightSideMenu.js
@@ -4,7 +4,7 @@ import FlatButton from 'material-ui/FlatButton';
 const styles = {
   textStyle: {
     fontFamily: 'Roboto',
-    fontSize: '1.2rem',
+    fontSize: '1.1rem',
     color: 'black',
     paddingTop: '8px',
     paddingRight: '20px'
