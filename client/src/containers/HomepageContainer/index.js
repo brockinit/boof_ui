95b5eea991f6d41374c8ed07732b9898ab37d6b2
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-07-16 16:09:09 -1000

    ✨ Metadata for homepage
     #6

--- client/src/containers/HomepageContainer/index.js
+++ client/src/containers/HomepageContainer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { GridList, GridTile } from 'material-ui/GridList';
 import { createClient } from 'contentful';
 import { Link } from 'react-router-dom';
@@ -93,6 +94,16 @@ class HomepageContainer extends React.Component {
   render() {
     return (
       <div>
+        <Helmet>
+          <title>Better Odds of Football</title>
+          <meta name="title" content="Better Odds of Football" />
+          <meta name="description" content="Giving you better odds through data science, machine learning, and years of football fandom." />
+          <meta property="og:title" content="Better Odds of Football" />
+          <meta property="og:url" content="https://betterodds.io" />
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="Giving you better odds through data science, machine learning, and years of football fandom." />
+          <meta property="og:image" content="/assets/boof-logo-metadata.png" />
+        </Helmet>
         <div style={styles.header}>
           <h1>Better Odds of Football</h1>
         </div>
