ec221c2e745584087a8ebe30bed211a14d854765
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-07-16 15:42:16 -1000

    Added SEO metadata

--- client/src/containers/AboutUsContainer/index.js
+++ client/src/containers/AboutUsContainer/index.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 import cat1 from '../../assets/cat-1.jpeg';
 import cat2 from '../../assets/cat-2.jpeg';
 import cat3 from '../../assets/cat-3.jpeg';
 
 const styles = {
-  AboutUsContainer: {
-    border: '10px solid blue'
-  },
   header: {
     paddingLeft: '3rem',
     paddingTop: '3rem',
     marginTop: '5rem',
     fontFamily: 'Roboto',
     fontSize: '3rem',
-    fontWeight: 'bold',
+    fontWeight: 'bold'
   },
   headerSubText: {
     fontSize: '1.7rem',
@@ -39,16 +37,25 @@ const styles = {
   imageBar: {
     display: 'flex',
     justifyContent: 'center',
-    border: '10px solid red',
     flexWrap: 'wrap'
   }
 }
 
-class AboutUs extends Component {
+class AboutUsContainer extends Component {
 
   render() {
     return (
       <div style={styles.AboutUsContainer}>
+        <Helmet>
+          <title>Better Odds of Football - About Us</title>
+          <meta name="title" content="Better Odds of Football - About Us" />
+          <meta name="description" content="Giving you better odds through data science, machine learning, and years of football fandom." />
+          <meta property="og:title" content="Better Odds of Football - About Us" />
+          <meta property="og:url" content="https://betterodds.io/about" />
+          <meta property="og:type" content="website" />
+          <meta property="og:description" content="Giving you better odds through data science, machine learning, and years of football fandom." />
+          <meta property="og:image" content="/assets/boof-logo-metadata.png" />
+        </Helmet>
         <div style={styles.header}>
           <h1>About Us</h1>
         </div>
@@ -62,9 +69,9 @@ class AboutUs extends Component {
           <p>Get to know us. Now.</p>
         </div>
         <div style={styles.imageBar}>
-          <img src={cat1}></img>
-          <img src={cat2}></img>
-          <img src={cat3}></img>
+          <img src={cat1} alt="Better Odds of Football Cat"></img>
+          <img src={cat2} alt="Better Odds of Football Persian"></img>
+          <img src={cat3} alt="Better Odds of Football Mr.Kitty"></img>
         </div>
         <div style={styles.aboutBody}>
           <h2 style={styles.h2}>Brock</h2>
@@ -73,9 +80,9 @@ class AboutUs extends Component {
           <p>As a lifelong Cleveland Browns fan and aspiring college professor, Sal knows all about the long road. Playing fantasy football for over a decade in five different leagues while simultaneously holding a job and completing multiple college degrees, Better Odds of Football is the latest of his analytic sporting endeavors. He hopes to spread his knowledge of sports, sports statistics, and sports phenomena across the internet like avocado toast, and enjoys football so much he is currently writing a thesis paper on the topic. Sal also loves a good map.</p>
           <h2 style={styles.h2}>Andrea</h2>
           <p>As the jack of all trades and master of none, Andrea is a noob developer and dabbles in digital advertising, product development, and project management. As the resident grandmother of the group, she has played fantasy football for a straight 16 years and running and brings years of wisdom and bullshit luck to the field. When not crushing the boys in fantasy football, she is a fan of adventure and has traveled solo to over 10 countries. Andrea Enjoys a good ear of corn and is still going to send it.</p>
-      </div>
+        </div>
       </div>
     )
   }
 }
-export default AboutUs;
+export default AboutUsContainer;
