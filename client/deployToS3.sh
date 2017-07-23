33c680ece9d2da4d4a6534b0370e5448e1ba0ab1
brockinit <brocklanoza@gmail.com>
2017-07-23 11:17:37 -1000

    Change bucket name in deploy script

--- client/deployToS3.sh
+++ client/deployToS3.sh
@@ -4,4 +4,4 @@
 npm run build
 
 # Copy the contents of the build folder into your S3 bucket and make them publicly available
-aws s3 sync build/ s3://better-odds --delete --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 sync build/ s3://www.betterodds.io --delete --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
