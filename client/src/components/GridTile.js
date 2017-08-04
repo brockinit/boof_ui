a68c8b615e19ed9872a868ef9f0315b880ddf22b
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-04 07:33:37 -1000

    💄 Background images are showing up, yay

--- client/src/components/GridTile.js
+++ client/src/components/GridTile.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const GridTile = props => {
+  // const imageUrl = require(`${props.image}`)
+  console.log(props.image, 'props image');
+
   return (
     <a className="single-gridtile" href="/">
-      <div className="gridcard-image"></div>
       <div className="gridcard-text">
         <h2>{props.title}</h2>
-        <p>Sample text over the image</p>
       </div>
-
+      <div className="gridcard-image" style={{ backgroundImage: `url(${props.image})` }} />
     </a>
   );
 };
