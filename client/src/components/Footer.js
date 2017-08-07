3f7b5b0f714a516b00dd77c19f2fe518a6a04f62
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-06 20:09:42 -1000

    🎨 Footer structure fixed, styles added

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,55 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-const styles = {
-  footerContainer: {
-    fontFamily: 'Roboto',
-    fontSize: '0.95rem',
-    height: '12rem',
-    lineHeight: '12rem',
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  footer: {
-    paddingLeft: '1rem',
-    paddingRight: '1rem',
-    textDecoration: 'none',
-    color: 'gray',
-  },
-};
 
 const Footer = () => {
   return (
-    <div className="footer-container" style={styles.footerContainer}>
-      <div className="footer">
-        <Link to="/" style={styles.footer}>
-          Home
-        </Link>
-      </div>
-      <div className="footer">
-        <Link to="/about" style={styles.footer}>
-          Contact
-        </Link>
-      </div>
-      <div className="footer">
-        <Link to="/" style={styles.footer}>
-          Articles
-        </Link>
-      </div>
-      <div className="footer">
-        <Link to="/privacy-policy" style={styles.footer}>
-          Privacy
-        </Link>
-      </div>
-      <div className="footer">
-        <Link to="/terms-conditions" style={styles.footer}>
-          Terms
-        </Link>
-      </div>
-      <div className="footer">
-        <Link to="/about" style={styles.footer}>
-          About
-        </Link>
+    <div className="footer-container">
+      <div className="footer-container-inner">
+        <div className="footer-item">
+          <a href="/">
+            <p>Home</p>
+          </a>
+        </div>
+        <div className="footer-item">
+          <a href="about">
+            <p>About Us</p>
+          </a>
+        </div>
       </div>
     </div>
   );
