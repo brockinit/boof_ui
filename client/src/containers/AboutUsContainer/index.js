6a83248fe15aec71341f993b72d7c887f2da31f8
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-06 19:57:49 -1000

    🎨 About Us page structured according to rest of site and styled"

--- client/src/containers/AboutUsContainer/index.js
+++ client/src/containers/AboutUsContainer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Metadata from '../../components/Metadata';
 import PageHeader from '../../components/PageHeader';
 import PageHeaderSubText from '../../components/PageHeaderSubText';
 import PageIntroText from '../../components/PageIntroText';
@@ -6,10 +7,20 @@ import AboutUsBody from '../../components/AboutUsBody';
 
 const AboutUsContainer = () => {
   return (
-    <div>
-      <PageHeader title="About Us" />
-      <PageHeaderSubText text="Giving you better odds through data science, machine learning, and years of football fandom." />
-      <PageIntroText text="Get to know us. Now." />
+    <div className="about-us-container outer">
+      <Metadata
+        title="About Us"
+        description="Giving you better odds through data science, machine learning, and years of football fandom."
+        url="https://www.betterodds.io/about"
+        image="/assets/boof-logo-metadata.png"
+      />
+      <div className="header-container-outer">
+        <div className="header-container div-container">
+          <PageHeader title="About Us" />
+          <PageHeaderSubText text="Giving you better odds through data science, machine learning, and years of football fandom." />
+          <PageIntroText text="Get to know us. Now." />
+        </div>
+      </div>
       <AboutUsBody />
     </div>
   );
