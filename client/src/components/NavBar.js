f625588d3b4e156f78c01b32f3bd7cff0a499247
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-06 19:33:51 -1000

    🎨 Nav Bar Added
    Structural changes to nav bar and article pages and styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,11 +5,32 @@ import Logo from '../assets/boof-logo-colored.svg';
 const NavBar = () => {
   return (
     <div className="navbar-container">
-      <img
-        src={Logo}
-        alt="Better Odds of Football Logo"
-        height="60px"
-      />
+      <div className="logo-container">
+        <a href="/">
+          <img
+            src={Logo}
+            alt="Better Odds of Football Logo"
+            height="60px"
+          />
+        </a>
+      </div>
+      <div className="menu-container">
+        <div className="menu-item">
+          <a href="/">
+            <p>Home</p>
+          </a>
+        </div>
+        <div className="menu-item">
+          <a href="about">
+            <p>About Us</p>
+          </a>
+        </div>
+        <div className="menu-item">
+          <a href="article/cleveland-browns-quarterback-madness-part-1">
+            <p>Articles</p>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
