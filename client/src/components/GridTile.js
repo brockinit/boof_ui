30e30458bdde48b852d4cb8967b4656e12518f90
brockinit <brocklanoza@gmail.com>
2017-08-06 21:52:58 -1000

    Parse article date

--- client/src/components/GridTile.js
+++ client/src/components/GridTile.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import moment from 'moment';
 
 const GridTile = props => {
   return (
     <a className="gridtile-container" href={`article/${props.slug}`}>
       <div className="gridtile-text">
-        <h2>{props.title}</h2>
-        <p>{props.author}</p>
-        <p>{props.date}</p>
+        <h2>
+          {props.title}
+        </h2>
+        <p>
+          {props.author}
+        </p>
+        <p>
+          {moment(props.date).format('LL')}
+        </p>
       </div>
-      <div className="gridtile-image" style={{ backgroundImage: `url(${props.image})` }} />
+      <div
+        className="gridtile-image"
+        style={{ backgroundImage: `url(${props.image})` }}
+      />
     </a>
   );
 };
