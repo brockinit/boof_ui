9003daca2e4369d6d30775f1d3d9f4507d6bcb6c
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-09 11:29:45 -1000

    ✨ Added contact form component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,40 +5,42 @@ class ContactForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
-    };
-    this.getAllPosts = this.getAllPosts.bind(this);
+      Email: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeEmail=this.handleChangeEmail.bind(this);
   }
 
-  getAllPosts() {
-
-    return new Promise((resolve, reject) => {
-      function reqListener(){
-        resolve(JSON.parse(this.responseText));
-      }
-      let oReq = new XMLHttpRequest();
-      oReq.open('GET', 'https://us16.api.mailchimp.com/3.0')
-      oReq.setRequestHeader('Access-Control-Allow-Origin':'*', 'Authorization', 'Basic ' + btoa('username:cb2a47bd962ed39eced32293b05af3a4-us16'), 'Content-Type', 'application/json')
-      oReq.addEventListener('load', reqListener)
-      oReq.send()
+  handleChangeEmail(event){
+    this.setState({
+      Email : event.target.value
     })
   }
 
-  componentWillMount(){
-    this.getAllPosts()
-    .then((data) => {
-      this.setState({
-        posts: data
-      })
+  handleSubmit(event) {
+    event.preventDefault();
+    return new Promise((resolve, reject) => {
+      function reqListener(){
+        console.log(this, 'this');
+      }
+      let oReq = new XMLHttpRequest();
+      oReq.open('POST', 'https://us16.api.mailchimp.com/3.0/lists/96d472b10a/members')
+      oReq.setRequestHeader('Access-Control-Allow-Origin':'*', 'Authorization', 'Basic ' + btoa('username:cb2a47bd962ed39eced32293b05af3a4-us16'), 'Content-Type', 'application/json')
+      oReq.addEventListener('load', reqListener)
+      oReq.send({ email_address : "takamiya.andrea2@gmail.com", status : "subscribed" })
     })
   }
 
   render() {
-    console.log(this.state.posts);
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <div id="contact-us-container">
+        <h1>Contact Us</h1>
+          <div className="generalForm">
+          <form onSubmit={this.handleSubmit}>
+            <input type='text' onChange={this.handleChangeEmail} placeholder="Email Address" name='Email' />
+            <input className="contact-us-submit" type="submit" value="Contact Us" />
+          </form>
         </div>
       </div>
     );
