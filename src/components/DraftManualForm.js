c654617890757ab487ec516e7096bf0033e28bf6
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-14 22:53:33 -1000

    Added a draft manual container page and form

---
+++ src/components/DraftManualForm.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import $ from "jquery";
+import { MAILCHIMP_ENDPOINT } from "../constants";
+import Notice from "./Notice";
+
+class DraftManualForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailAddress: "",
+      firstName: "",
+      lastName: "",
+      contactUsNote: "",
+      sentStatus: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const options = {
+      url: MAILCHIMP_ENDPOINT,
+      data: JSON.stringify({
+        emailAddress: this.state.emailAddress,
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        contactUsNote: this.state.contactUsNote
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    $.post(options).then(data => {
+      if (data.statusCode === 200) {
+        this.setState({
+          sentStatus: "sent",
+          emailAddress: "",
+          firstName: "",
+          lastName: "",
+          contactUsNote: ""
+        });
+      } else {
+        this.setState({
+          sentStatus: "error"
+        });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="contact-us-container">
+        <div className="contact-us-inner">
+          {(() => {
+            switch (this.state.sentStatus) {
+              case "sent":
+                return (
+                  <Notice
+                    status="Your message has been sent! We'll contact you shortly"
+                    statusClass="success-message"
+                    noticeContainerClass="notice-container-success"
+                  />
+                );
+              case "error":
+                return (
+                  <Notice
+                    status="An error occured, please try again"
+                    noticeContainerClass="notice-container-error"
+                    statusClass="error-message"
+                  />
+                );
+              default:
+                return "";
+            }
+          })()}
+          <div className="form-container">
+            <form onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                onChange={this.handleChange}
+                placeholder="Email Address"
+                name="emailAddress"
+                value={this.state.emailAddress}
+                className="input"
+              />
+              <input
+                type="text"
+                onChange={this.handleChange}
+                placeholder="First Name"
+                name="firstName"
+                value={this.state.firstName}
+                className="input"
+              />
+              <input
+                type="text"
+                onChange={this.handleChange}
+                placeholder="Last Name"
+                name="lastName"
+                value={this.state.lastName}
+                className="input"
+              />
+              <textarea
+                onChange={this.handleChange}
+                placeholder="Write us a note"
+                name="contactUsNote"
+                value={this.state.contactUsNote}
+                className="message"
+              />
+              <input
+                className="contact-button"
+                type="submit"
+                value="Get Your 2017 Draft Manual"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DraftManualForm;
---
+++ src/containers/DraftManualContainer/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Metadata from "../../components/Metadata";
+import PageHeader from "../../components/PageHeader";
+import PageHeaderSubText from "../../components/PageHeaderSubText";
+import PageIntroText from "../../components/PageIntroText";
+import DraftManualForm from "../../components/DraftManualForm";
+
+const ContactUsContainer = () => {
+  return (
+    <div className="contact-us-container outer">
+      <Metadata
+        title="Get Your 2017 Draft Manual"
+        description="Giving you better odds through data science, machine learning, and years of football fandom."
+        url="https://www.betterodds.io/about"
+        image="/assets/boof-logo-metadata.png"
+      />
+      <div className="header-container-outer">
+        <div className="header-container div-container">
+          <PageHeader title="Get Your 2017 Draft Manual" />
+          <PageHeaderSubText text="Fantasy Football 2017 Draft Manual, complete with individual defensive player rankings and suggestions" />
+          <PageIntroText text="Enter your information below and we'll send you a PDF of the 2017 Better Odds of Football draft manual." />
+        </div>
+      </div>
+      <DraftManualForm />
+    </div>
+  );
+};
+
+export default ContactUsContainer;
