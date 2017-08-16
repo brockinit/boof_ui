d5ea4ffe4183e0a8afd8b6f49107cd2f5cf1c6f7
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-15 22:48:27 -1000

    Added to each of the article pages

--- src/containers/ArticleContainer/index.js
+++ src/containers/ArticleContainer/index.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import Metadata from '../../components/Metadata';
-import ArticleHeader from '../../components/ArticleHeader';
-import marked from 'marked';
-import moment from 'moment';
-import { ajax } from 'jquery';
-import { BLOG_POST_ENDPOINT } from '../../constants';
+import React, { Component } from "react";
+import Metadata from "../../components/Metadata";
+import ArticleHeader from "../../components/ArticleHeader";
+import DraftManualAd from "../../components/DraftManualAd";
+import marked from "marked";
+import moment from "moment";
+import { ajax } from "jquery";
+import { BLOG_POST_ENDPOINT } from "../../constants";
 
 class ArticleContainer extends Component {
   constructor(props) {
@@ -16,12 +17,12 @@ class ArticleContainer extends Component {
       articleShortDescription: null,
       author: null,
       dateCreated: null,
-      slug: null,
+      slug: null
     };
   }
 
   componentDidMount() {
-    const postId = this.props.location.pathname.split('/').pop();
+    const postId = this.props.location.pathname.split("/").pop();
     return ajax(BLOG_POST_ENDPOINT).then(items => {
       for (var i = 0; i < items.length; i++) {
         if (items[i].fields.slug === postId) {
@@ -30,8 +31,8 @@ class ArticleContainer extends Component {
             articleTitle: items[i].fields.articleTitle,
             articleShortDescription: items[i].fields.articleShortDescription,
             author: items[i].fields.author,
-            dateCreated: moment(items[i].fields.dateCreated).format('LL'),
-            slug: items[i].fields.slug,
+            dateCreated: moment(items[i].fields.dateCreated).format("LL"),
+            slug: items[i].fields.slug
           });
         }
       }
@@ -40,31 +41,34 @@ class ArticleContainer extends Component {
 
   parseMarkdown() {
     return {
-      __html: marked(this.state.articleBody, { sanitize: true }),
+      __html: marked(this.state.articleBody, { sanitize: true })
     };
   }
 
   render() {
     return (
-      <div className="article-container outer">
-        <div className="article-container-inner">
-          <Metadata
-            title={this.state.articleTitle}
-            description={this.state.articleShortDescription}
-            url={`https://www.betterodds.io/${this.state.slug}`}
-            image="/assets/boof-logo-metadata.png"
-          />
-          <ArticleHeader
-            title={this.state.articleTitle}
-            author={this.state.author}
-            date={this.state.dateCreated}
-          />
-          {this.state.articleBody &&
-            <div
-              className="article-body"
-              dangerouslySetInnerHTML={this.parseMarkdown()}
-            />}
+      <div>
+        <div className="article-container outer">
+          <div className="article-container-inner">
+            <Metadata
+              title={this.state.articleTitle}
+              description={this.state.articleShortDescription}
+              url={`https://www.betterodds.io/${this.state.slug}`}
+              image="/assets/boof-logo-metadata.png"
+            />
+            <ArticleHeader
+              title={this.state.articleTitle}
+              author={this.state.author}
+              date={this.state.dateCreated}
+            />
+            {this.state.articleBody &&
+              <div
+                className="article-body"
+                dangerouslySetInnerHTML={this.parseMarkdown()}
+              />}
+          </div>
         </div>
+        <DraftManualAd />
       </div>
     );
   }
