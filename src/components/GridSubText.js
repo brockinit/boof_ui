aa9806186bb534437b1375dd93bfde056c26748e
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-28 20:51:56 -1000

    Added visible article titles

---
+++ src/components/GridSubText.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
+
+const GridSubText = props => {
+  return (
+    <div className="gridtile-subtext-container">
+      <div className="gridtile-text">
+          <Link to={`articles/${props.slug}`}>
+            <h2>
+              {props.title}
+            </h2>
+          </Link>
+          <hr />
+        <p>
+          {props.author}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GridSubText;
